48c7ea9da9a3b8f228f568663329c1094d5b362d
Jorge Saxon <jorgesaxon1234@gmail.com>
2023-01-26 18:17:33 -0600

    Optimizations

--- src/lib/BoardLogic.svelte
+++ src/lib/BoardLogic.svelte
@@ -1,25 +1,39 @@
 <script lang="ts">
   import { each, onMount } from "svelte/internal";
   import { fly } from "svelte/transition";
-  import {io} from "socket.io-client"
-  const socket = io("https://connectfourserver-2ucedebk7a-wn.a.run.app")
-
-  let BoardArray = []
+  import { io } from "socket.io-client";
+  const socket = io("https://connectfourserver-2ucedebk7a-wn.a.run.app");
+  import axios from "axios";
+  let BoardArray = [];
   let player = 1;
   let maxHeight = [5, 5, 5, 5, 5, 5];
-  socket.on("get", (board)=>{
-    BoardArray = board.board
-    maxHeight = board.maxHeight
-    console.log(maxHeight)
+  let currentPlayer = "";
+  socket.on("get", (board) => {
+    BoardArray = board.board;
+    maxHeight = board.maxHeight;
+    currentPlayer = getColor(board.currentPlayer === 1 ? 2 : 1);
+    console.log(maxHeight);
+  });
+  let ourPlayer = ""
+  async function getPlayer(id) {
+    const players = await axios.get("http://localhost:3000", {
 
-  })
-  const key = Math.floor(Math.random()*1000)
-  socket.emit("join",{
-    key: key
-  })
-  socket.on("end", ()=>{
-    state.winner = null
-  })
+    })
+    const index = await players.data.arr.indexOf(id)
+    console.log(index)
+    ourPlayer = await getColor( index+1 ) === "blue" ? "red" : "blue"
+    if(index === -1){
+      location.reload()
+    }
+  }
+  socket.on("connect", () => {
+    const id = socket.id;
+    getPlayer(id)
+  });
+
+  socket.on("end", () => {
+    state.winner = null;
+  });
   const horizontalCheck = [
     [0, 0],
     [0, 1],
@@ -48,12 +62,13 @@
     if (num === 2) {
       return "blue";
     }
+    return "nulle"
   }
 
   function handleClick(y, x) {
     if (maxHeight[y] !== -1) {
       player = (player % 2) + 1;
-      socket.emit("move", [maxHeight[y],y,player,key])
+      socket.emit("move", [maxHeight[y], y, player, socket.id]);
       maxHeight[y] -= 1;
     }
   }
@@ -64,7 +79,7 @@
   function checkWin(x, y) {
     let results = [];
     // Get neighbors
-    let won
+    let won;
     results[0] = horizontalCheck.map((offset) => {
       return BoardArray?.[x + offset[0]]?.[y + offset[1]] || 0;
     });
@@ -85,23 +100,23 @@
     won = results.some((el) => el !== false);
     if (won) {
       const winner = numberToName[BoardArray[x][y]];
-      socket.emit("end", true)
+      socket.emit("end", true);
       return winner;
     } else {
       return false;
     }
   }
   $: {
-    if(BoardArray === undefined){
-      BoardArray= [
-  [ 0, 0, 0, 0, 0, 0 ],
-  [ 0, 0, 0, 0, 0, 0 ],
-  [ 0, 0, 0, 0, 0, 0 ],
-  [ 0, 0, 0, 0, 0, 0 ],
-  [ 0, 0, 0, 0, 0, 0 ],
-  [ 0, 0, 0, 0, 0, 0 ]
-]
-      }
+    if (BoardArray === undefined) {
+      BoardArray = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+      ];
+    }
     for (let x = 0; x < BoardArray.length; x++) {
       for (let y = 0; y < BoardArray[0].length; y++) {
         let win = checkWin(y, x);
@@ -119,7 +134,17 @@
     console.log(state.winner);
   }
 </script>
-
+  {ourPlayer}
+	{#if currentPlayer === ourPlayer}
+	<h1>
+	  Its your turn make a move
+	</h1>
+	{/if}
+	{#if currentPlayer !== ourPlayer}
+	  <h1>
+	   Wait For the other player.
+	  </h1>
+	{/if}
 <div class="board">
   {#each BoardArray as arr, y}
     <div class="row">
@@ -131,7 +156,7 @@
   {/each}
 </div>
 {#if state.winner !== null}
-  <h1 in:fly="{{ x: -1200, duration: 1000 }}">
+  <h1 in:fly={{ x: -1200, duration: 1000 }}>
     <span class={state.winner}>{state.winner}</span>
     Won!
   </h1>
